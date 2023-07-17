7c139af188c505b7818d3305a2da6989ae0a1d1c
מוני ברוך <monybaruch@gmail.com>
2023-07-17 04:52:55 +0300

    adding JS tasks 1.1 and 2.3

--- 1.1.js
+++ 1.1.js
@@ -1,2 +1,8 @@
 // Complete the method that takes a boolean value and return a "Yes" string for true, or a "No"
 // string for false.
+
+const booleanToStr = (boolean) => {
+  return boolean === true ? 'Yes' : 'No';
+};
+
+console.log(booleanToStr(false));
