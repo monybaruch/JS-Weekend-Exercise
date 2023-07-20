39f59496eef099c6c04c494bf02ce1fc9f90eb0f
מוני ברוך <monybaruch@gmail.com>
2023-07-20 23:11:19 +0300

    adding solution to 2.5

---
+++ 2.5.js
@@ -0,0 +1,16 @@
+// Ex2.5 - Summation
+// Write a program that finds the summation of every number from 1 to num. The number will
+// always be a positive integer greater than 0.
+// For example:
+// summation(2) -> 3
+// 1 + 2
+// summation(8) -> 36
+// 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8
+
+const nums = [1, 2, 3, 4, 5, 6, 7, 8];
+
+let numbersTotal = nums.reduce((total, currentVal) => {
+  return total + currentVal;
+}, 0);
+
+console.log(numbersTotal);
